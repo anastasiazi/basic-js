5321fd40a6619a895138cb24fc8f86acb1952cc4
anastasiazi <anastasiiiazi@gmail.com>
2021-03-07 20:15:38 +0200

    fest: function dreamTeam is implemented

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let newName = '';
+
+  if ( members === undefined || members === null || members.length === 0) {
+    return false;
+  } else {
+    for ( i = 0; i < members.length; i++ ) {
+      if ( typeof(members[i]) === 'string') {
+          newName += (members[i]).trim().toUpperCase().charAt(0);
+      }
+  }
+
+  return (((Array.from(newName))).sort()).join('');
+  }
 };
