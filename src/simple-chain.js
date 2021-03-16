96dd3e3530483e45499b1480c0672365eee6904c
anastasiazi <anastasiiiazi@gmail.com>
2021-03-16 12:50:27 +0200

    feat: simple-chain function was implemented

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  chains: [],
+
+    getLength() {
+      return this.chains.length;
+    },
+
+    addLink(value) {
+      if (value !== undefined) {
+        this.chains.push('' + value);
+      }
+        return this;
+      },
+
+    removeLink(position) {
+        if (typeof position !== 'number' || position < 0) {
+          this.chains.splice(0)
+          throw new Error
+        } else { 
+          this.chains.splice(position - 1, 1);
+        }
+        return this;
+      },
+
+    reverseChain() {
+        this.chains.reverse()
+        return this;
+      },
+
+    finishChain() {
+        let finishedChain = '( ' + this.chains.join(' )~~( ') + ' )';
+        this.chains.splice(0)
+        return finishedChain;
+
+      }
 };
 
 module.exports = chainMaker;
